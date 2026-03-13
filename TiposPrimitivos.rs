33688a2fa40401f14eb6565e2505bc4f28d8c5df
Carlos Ivan Mamédio <carlos.ivan1997@outlook.com>
2026-03-13 11:15:50 -0300

    add examples of Rust primitive types

---
+++ TiposPrimitivos.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let inteiro_com_sinal: i32 = -42;
+    let inteiro_sem_sinal: u32 = 42;
+    let ponto_flutuante: f64 = 3.14159;
+    let booleano: bool = true;
+    let caractere: char = 'R';
+    
+    println!("i32: {}", inteiro_com_sinal);
+    println!("u32: {}", inteiro_sem_sinal);
+    println!("f64: {}", ponto_flutuante);
+    println!("bool:{}", booleano);
+    println!("char: {}", caractere);
+    
+}
