bc6f80744a774c66bc673ffebfbb26cc50a9f152
Carlos Ivan Mamédio <carlos.ivan1997@outlook.com>
2026-03-13 11:07:22 -0300

    Add files via upload

---
+++ VariaveisMutabilidade.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Váriavel imutavel
+    let imutavel = 10;
+    println!("imutavel: {}", imutavel);
+    
+    // Váriavel mutavel
+    let mut mutavel = 20;
+    println!("Mutável (original): {}", mutavel);
+    mutavel = 30;
+    println("Mutável (modificada): {}", mutavel);
+    
+}
